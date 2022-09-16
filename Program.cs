157a6aea03d88848e32c6fa75e0bfb16a40db306
Nikolaj <mr.nik.simonov@mail.ru>
2022-09-16 23:42:37 +0700

    Second program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
-Console.Write("Введите первое число: ");
+/*Console.Write("Введите первое число: ");
 int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
@@ -16,4 +16,31 @@ if (number1 < number2)
 else
 {
     Console.WriteLine($"Большее число {number1}, меньшее число {number2}.");
+}*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+/*//Первый вариант решения
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
 }
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine($"Ьольшее число из трех равно {max}");*/
+/*//Второй вариант решения с использованием библиотек
+int max = Math.Max(number1, Math.Max(number2, number3));
+Console.WriteLine($"Ьольшее число из трех равно {max}");*/ 
